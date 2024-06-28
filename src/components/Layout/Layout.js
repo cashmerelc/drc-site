144e7e8414ca24eb17e2b7265a30b8cc4ce154b3
Derek Myers <cashmerelc@gmail.com>
2024-06-28 11:45:17 +0200

    Add additional icon styling and tooltips to navbar, plus remove default Nextjs content

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,7 +4,7 @@ export default function Layout({ children }) {
   return (
     <>
       <NavBar />
-      <main>{children}</main>
+      <main className="bg-gray-800 h-screen">{children}</main>
     </>
   );
 }
--- src/components/Nav/NavBar.js
+++ src/components/Nav/NavBar.js
@@ -3,11 +3,17 @@ import { FaHome, FaInfo, FaCode } from "react-icons/fa";
 export default function NavBar() {
   return (
     <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 shadow-lg">
-      <NavBarIcon icon={<FaHome />} />
-      <NavBarIcon icon={<FaInfo />} />
-      <NavBarIcon icon={<FaCode />} />
+      <NavBarIcon icon={<FaHome />} text="Home" />
+      <NavBarIcon icon={<FaInfo />} text="About Me" />
+      <NavBarIcon icon={<FaCode />} text="Projects" />
     </div>
   );
 }
 
-const NavBarIcon = ({ icon }) => <div className="navbar-icon">{icon}</div>;
+const NavBarIcon = ({ icon, text }) => (
+  <div className="navbar-icon group">
+    {icon}
+
+    <span class="navbar-tooltip group-hover:scale-100">{text}</span>
+  </div>
+);
